d39c723506b48cf8f5902e8b217c5fcb5e52b421
AdityaPratap-129 <101492048+AdityaPratap-129@users.noreply.github.com>
2022-04-09 16:38:36 +0530

    Added solution - LeetHub

---
+++ 1. Print 1 To N Without Loop - GFG/1.-print-1-to-n-without-loop.c
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+void printNos(int N)
+{
+    //Your code here
+    if(N>=1)
+        {
+            printNos(N-1);
+            printf("%d ",N );
+        }
+        else
+        {
+            return;
+        }
+    
+}
+
+// { Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+    
+    //taking testcases
+    scanf("%d", &T);
+    
+    while(T--)
+    {
+        int N;
+        
+        //input N
+        scanf("%d", &N);
+        //calling printNos() function
+        printNos(N);
+        printf("\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
