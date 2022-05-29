d9c7391002bb75843de9d9805dbc40c9b50954ba
Aditya Pratap Singh <101492048+AdityaPratap-129@users.noreply.github.com>
2022-05-29 12:35:53 +0530

    Added solution - LeetHub

---
+++ 15. Subarray range with given sum - GFG/15.-subarray-range-with-given-sum.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(int arr[], int n, int sum)
+    {
+        //Your code here
+        int s=0,c=0;
+        unordered_map<int,int> h;
+        h[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            s=s+arr[i];
+            c=c+h[s-sum];
+            h[s]++;
+        }
+        
+        return c;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,sum=0;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    cin>>sum;
+	    Solution obj;
+	    cout<<obj.subArraySum(arr, n, sum)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
