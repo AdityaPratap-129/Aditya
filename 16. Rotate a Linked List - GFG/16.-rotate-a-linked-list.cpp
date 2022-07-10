14628891ef9021d7c4c8ec4c5f15b073e3777bd4
Aditya Pratap Singh <101492048+AdityaPratap-129@users.noreply.github.com>
2022-07-10 15:52:55 +0530

    Added solution - LeetHub

---
+++ 16. Rotate a Linked List - GFG/16.-rotate-a-linked-list.cpp
@@ -0,0 +1,107 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+            Node *ptr=head;
+            Node *cur=head;
+            Node *prev=head;
+            int c=0,m=1;
+            while(c!=k && cur!=NULL)
+            {
+                prev=cur;
+                c++;
+                cur=cur->next;
+            }
+            while(ptr->next!=NULL)
+            {
+                m++;
+                ptr=ptr->next;
+            }
+            if(m==k)
+            {
+                return head;
+            }
+            prev->next=NULL;
+            ptr->next=head;
+            head=cur;
+            return head;
+        
+    }
+    
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+  // } Driver Code Ends
